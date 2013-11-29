0248ee7544835fea424a409d9728f63c32846da8
Jon Ivmark <jon.ivmark@klarna.com>
2013-11-29 11:05:14 +0100

    Created a fluent filter builder for properties.

---
+++ graphit-core/src/main/java/org/jon/ivmark/graphit/core/graph/traversal/PropertiesFilterBuilder.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2012 Jon Ivmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jon.ivmark.graphit.core.graph.traversal;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.jon.ivmark.graphit.core.properties.Properties;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public final class PropertiesFilterBuilder {
+
+    private final List<Predicate<Properties>> filters;
+
+    private PropertiesFilterBuilder() {
+        this.filters = new ArrayList<Predicate<Properties>>();
+        filters.add(Predicates.<Properties>notNull());
+    }
+
+    public static PropertyFilterBuilder where(String key) {
+        return new PropertyFilterBuilder(new PropertiesFilterBuilder(), key);
+    }
+
+    public PropertyFilterBuilder and(String key) {
+        return new PropertyFilterBuilder(this, key);
+    }
+
+    public Predicate<Properties> build() {
+        return Predicates.and(filters);
+    }
+
+    private PropertiesFilterBuilder addFilter(Predicate<Properties> filter) {
+        filters.add(filter);
+        return this;
+    }
+
+    public static final class PropertyFilterBuilder {
+
+        private final PropertiesFilterBuilder builder;
+        private final String key;
+
+        private PropertyFilterBuilder(PropertiesFilterBuilder builder, String key) {
+            this.builder = builder;
+            this.key = key;
+        }
+
+        public PropertiesFilterBuilder isNull() {
+            return builder.addFilter(PropertyFilter.isNull(key));
+        }
+
+        public PropertiesFilterBuilder notNull() {
+            return builder.addFilter(PropertyFilter.notNull(key));
+        }
+
+        public PropertiesFilterBuilder equalTo(Object target) {
+            return builder.addFilter(PropertyFilter.equalTo(key, target));
+        }
+
+        public PropertiesFilterBuilder notEqualTo(Object target) {
+            return builder.addFilter(PropertyFilter.notEqualTo(key, target));
+        }
+
+        public <C extends Comparable<C>> PropertiesFilterBuilder lessThan(C limit) {
+            return builder.addFilter(PropertyFilter.lessThan(key, limit));
+        }
+
+        public <C extends Comparable<C>> PropertiesFilterBuilder lessThanOrEqualTo(C limit) {
+            return builder.addFilter(PropertyFilter.lessThanOrEqual(key, limit));
+        }
+
+        public <C extends Comparable<C>> PropertiesFilterBuilder greaterThan(C limit) {
+            return builder.addFilter(PropertyFilter.greaterThan(key, limit));
+        }
+
+        public <C extends Comparable<C>> PropertiesFilterBuilder greaterThanOrEqualTo(C limit) {
+            return builder.addFilter(PropertyFilter.greaterThanOrEqual(key, limit));
+        }
+
+        public PropertiesFilterBuilder matches(Pattern pattern) {
+            return builder.addFilter(PropertyFilter.matches(key, pattern));
+        }
+
+        public <C> PropertiesFilterBuilder in(Collection<C> target) {
+            return builder.addFilter(PropertyFilter.in(key, target));
+        }
+
+        public <C> PropertiesFilterBuilder notIn(Collection<C> target) {
+            return builder.addFilter(PropertyFilter.notIn(key, target));
+        }
+    }
+}
---
+++ graphit-core/src/test/java/org/jon/ivmark/graphit/core/graph/traversal/PropertiesFilterBuilderTest.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright 2012 Jon Ivmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jon.ivmark.graphit.core.graph.traversal;
+
+import com.google.common.base.Predicate;
+import org.jon.ivmark.graphit.core.properties.Properties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static org.jon.ivmark.graphit.core.graph.traversal.PropertiesFilterBuilder.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PropertiesFilterBuilderTest {
+
+    @Mock
+    private Properties properties;
+
+    @Test
+    public void testIsNull() {
+        Predicate<Properties> filter = where("nonExisting").isNull().build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeIsNull() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").isNull().build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNotNull() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").notNull().build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeNotNull() {
+        Predicate<Properties> filter = where("nonExisting").notNull().build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testEqualTo() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").equalTo(1).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeEqualTo() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").equalTo(2).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNullDoesNotEqualToAnything() {
+        Predicate<Properties> filter = where("nonExisting").equalTo(1).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNotEqualTo() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").notEqualTo(2).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeNotEqualTo() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").notEqualTo(1).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNullNotEqualToEverything() {
+        Predicate<Properties> filter = where("nonExisting").notEqualTo(1).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testLessThan() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").lessThan(2).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeLessThan() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        assertFalse(where("existing").lessThan(0).build().apply(properties));
+        assertFalse(where("existing").lessThan(1).build().apply(properties));
+    }
+
+    @Test
+    public void testNullNotLessThanAnything() {
+        Predicate<Properties> filter = where("nonExisting").lessThan(2).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testLessThanOrEqual() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        assertTrue(where("existing").lessThanOrEqualTo(1).build().apply(properties));
+        assertTrue(where("existing").lessThanOrEqualTo(2).build().apply(properties));
+    }
+
+    @Test
+    public void testNegativeLessThanOrEqual() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").lessThanOrEqualTo(0).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNullNotLessThanOrEqualToAnything() {
+        Predicate<Properties> filter = where("nonExisting").lessThanOrEqualTo(2).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testGreaterThan() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").greaterThan(0).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeGreaterThan() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        assertFalse(where("existing").greaterThan(2).build().apply(properties));
+        assertFalse(where("existing").greaterThan(1).build().apply(properties));
+    }
+
+    @Test
+    public void testNullNotGreaterThanAnything() {
+        Predicate<Properties> filter = where("nonExisting").greaterThan(0).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testGreaterThanOrEqual() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        assertTrue(where("existing").greaterThanOrEqualTo(0).build().apply(properties));
+        assertTrue(where("existing").greaterThanOrEqualTo(1).build().apply(properties));
+    }
+
+    @Test
+    public void testNegativeGreaterThanOrEqual() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").greaterThanOrEqualTo(2).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNullNotGreaterThanOrEqualToAnything() {
+        Predicate<Properties> filter = where("nonExisting").greaterThanOrEqualTo(2).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testMatches() {
+        when(properties.getProperty("existing")).thenReturn("Hello world");
+        Pattern pattern = Pattern.compile("^Hello\\s\\w*$");
+        Predicate<Properties> filter = where("existing").matches(pattern).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeMatches() {
+        when(properties.getProperty("existing")).thenReturn("Hello big world");
+        Pattern pattern = Pattern.compile("^Hello\\s\\w*$");
+        Predicate<Properties> filter = where("existing").matches(pattern).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNullMatchesNothing() {
+        Pattern pattern = Pattern.compile("^Hello\\s\\w*$");
+        Predicate<Properties> filter = where("nonExisting").matches(pattern).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testIn() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").in(Arrays.asList(3, 2, 1)).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeIn() {
+        when(properties.getProperty("existing")).thenReturn(4);
+        Predicate<Properties> filter = where("existing").in(Arrays.asList(3, 2, 1)).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNullNotInAnything() {
+        Predicate<Properties> filter = where("nonExisting").in(Arrays.asList(3, 2, 1)).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNotIn() {
+        when(properties.getProperty("existing")).thenReturn(4);
+        Predicate<Properties> filter = where("existing").notIn(Arrays.asList(3, 2, 1)).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeNotIn() {
+        when(properties.getProperty("existing")).thenReturn(1);
+        Predicate<Properties> filter = where("existing").notIn(Arrays.asList(3, 2, 1)).build();
+        assertFalse(filter.apply(properties));
+    }
+
+    @Test
+    public void testNotInWithNull() {
+        Predicate<Properties> filter = where("nonExisting").notIn(Arrays.asList(3, 2, 1)).build();
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testCombination() {
+        when(properties.getProperty("anInt")).thenReturn(1);
+        when(properties.getProperty("aString")).thenReturn("Hello");
+
+        Predicate<Properties> filter = where("anInt").lessThan(2)
+                        .and("aString").equalTo("Hello")
+                        .and("nonExisting").isNull()
+                        .build();
+
+        assertTrue(filter.apply(properties));
+    }
+
+    @Test
+    public void testNegativeCombination() {
+        when(properties.getProperty("anInt")).thenReturn(1);
+        when(properties.getProperty("aString")).thenReturn("Hello");
+
+        Predicate<Properties> filter = where("anInt").lessThan(2)
+                .and("aString").equalTo("Hello world")
+                .and("nonExisting").isNull()
+                .build();
+
+        assertFalse(filter.apply(properties));
+    }
+}
